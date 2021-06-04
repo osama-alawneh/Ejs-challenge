98826e3328f59ef6dc5b4d93bcc53936e7548ab5
osama-alawneh <h.a.alawneh9@gmail.com>
2021-06-04 20:10:20 +0300

    deploying

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
-
 
 
 const posts =[];
@@ -12,6 +12,25 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+mongoose.connect("mongodb://localhost:27017/blogDB" , {useUnifiedTopology: true,useNewUrlParser:true ,useFindAndModify: false, });
+
+//Database models
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        minLength: 3,
+        maxLength: 50,
+        required: true
+    },
+    content: {
+        type: String,
+        minLength: 3,
+        maxLength: 2000,
+        required: true
+    }
+});
+
+const Post = mongoose.model("post" ,postSchema);
 
 //view engine
 app.set('view engine', 'ejs');
@@ -22,7 +41,13 @@ app.use(express.static("public"));
 
     // Get Home Route
 app.get('/' , (req,res) =>{
-   res.render('home' , {homeStartingContent : homeStartingContent , posts : posts});
+    Post.find()
+        .then(result =>{
+                res.render('home' , {homeStartingContent : homeStartingContent , posts : result});
+        })
+        .catch(err =>{
+            console.log(err);
+        });
 });
 
     //Get about Route
@@ -40,24 +65,26 @@ app.get('/compose' , (req,res) =>{
 });
 
 
-app.get('/posts/:title' , (req,res) =>{
-    const requestedTitle = _.lowerCase(req.params.title);
-
-    posts.forEach((post) =>{
-       if (_.lowerCase(post.title) === requestedTitle){
-           res.render('post' , {title : post.title , content : post.content});
-       }
-    });
+app.get('/posts/:postId' , (req,res) =>{
+    const requestedPostId = req.params.postId;
+    Post.findOne({_id: requestedPostId})
+        .then(result =>{
+            res.render('post' , {title : result.title , content : result.content});
+        })
+        .catch(err =>{
+            console.log(err);
+        });
 
 });
 
 
 app.post('/compose' , (req,res) =>{
-    const post = {
+
+    const post = new Post ({
         title : req.body.postTitle,
         content : req.body.postBody
-    };
-
+    });
+    post.save();
     posts.push(post);
     res.redirect('/');
 });
